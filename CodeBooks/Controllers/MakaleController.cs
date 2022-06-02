c0d6e2f4524eb1a9511dc59cccd41084d034c3d0
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2022-06-02 20:55:29 +0300

    makale basic işlemler

--- CodeBooks/Controllers/MakaleController.cs
+++ CodeBooks/Controllers/MakaleController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodeBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,48 @@ namespace CodeBooks.Controllers
     // en son [Authorize] ekle
     public class MakaleController : Controller
     {
+
+        Context c = new Context();
         public IActionResult Index()
         {
+            ViewBag.kategoriad = c.Kategoris.ToList();
+            
+            var degerler = c.Makales.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public IActionResult MakaleEkle()
+        {
+
+
+            List<SelectListItem> degerler = (from x in c.Kategoris.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.KategoriAd,
+                                                 Value = x.KategoriID.ToString()
+                                             }).ToList();
+
+            ViewBag.dgr = degerler;
+
             return View();
         }
+        [HttpPost]
+        public IActionResult MakaleEkle( Makale m)
+        {
+
+            var kat = c.Kategoris.Where(x => x.KategoriID == m.kategori.KategoriID).FirstOrDefault();
+            m.Tarih = "22.04.2021";
+            m.kategori = kat;
+            c.Makales.Add(m);
+            c.SaveChanges();
+
+
+
+            return RedirectToAction("Index", "Makale");
+        }
+
+
+
     }
 }
