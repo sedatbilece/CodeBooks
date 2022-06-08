fa5babc7ff1e84c4d722c87022fc1d737d306fe2
Mahmut Demir <61451173+mahmutdemr@users.noreply.github.com>
2022-06-08 11:58:23 +0300

    azure deploy2 https://codebooks2.azurewebsites.net mahmut

---
+++ CodeBooks/Migrations/20220608083122_azuredep2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodeBooks.Migrations
+{
+    public partial class azuredep2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
